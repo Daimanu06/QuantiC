c1192f5d657b81071592fe1e2aefcde5fbe9991f
Manu <emmanuel@diclemente.fr>
2017-07-08 20:00:38 +0200

    Translations (FR-EN) added

---
+++ translations/QuantiC.en.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>Banner</name>
+    <message>
+        <location filename="banner.cpp" line="96"/>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+</context>
+<context>
+    <name>BrowseFilesWidget</name>
+    <message>
+        <location filename="browsefileswidget.cpp" line="13"/>
+        <source>Browse</source>
+        <comment>Browse files</comment>
+        <translation>Browse</translation>
+    </message>
+</context>
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="dialogabout.cpp" line="18"/>
+        <source>Visit website</source>
+        <translation>Visit website</translation>
+    </message>
+    <message>
+        <location filename="dialogabout.cpp" line="22"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="dialogabout.cpp" line="25"/>
+        <source>%1 is a light, flexible and cross platform quantity converter.&lt;br /&gt;Version %2.</source>
+        <translation>%1 is a light, flexible and cross platform quantity converter.&lt;br /&gt;Version %2.</translation>
+    </message>
+</context>
+<context>
+    <name>DialogSettings</name>
+    <message>
+        <location filename="dialogsettings.cpp" line="56"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="59"/>
+        <source>Data</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="60"/>
+        <source>Window</source>
+        <translation>Window</translation>
+    </message>
+</context>
+<context>
+    <name>DialogTabData</name>
+    <message>
+        <location filename="dialogsettings.cpp" line="15"/>
+        <source>Data location</source>
+        <translation>Data location</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="16"/>
+        <source>Icons location</source>
+        <translation>Icons location</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="17"/>
+        <source>Any change will take effect after restarting the application.</source>
+        <translation>Any change will take effect after restarting the application.</translation>
+    </message>
+</context>
+<context>
+    <name>DialogTabWindow</name>
+    <message>
+        <location filename="dialogsettings.cpp" line="33"/>
+        <source>Keep window state</source>
+        <translation>Keep window state</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="34"/>
+        <source>Reset window</source>
+        <translation>Reset window</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="41"/>
+        <source>&amp;Application</source>
+        <translation>&amp;Application</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="44"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="42"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+</context>
+<context>
+    <name>StackedWidget</name>
+    <message>
+        <location filename="stackedwidget.cpp" line="15"/>
+        <source>No quantities found.</source>
+        <translation>No quantities found.</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/QuantiC.fr_FR.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="fr_FR">
+<context>
+    <name>Banner</name>
+    <message>
+        <location filename="banner.cpp" line="96"/>
+        <source>Home</source>
+        <translation>Accueil</translation>
+    </message>
+</context>
+<context>
+    <name>BrowseFilesWidget</name>
+    <message>
+        <location filename="browsefileswidget.cpp" line="13"/>
+        <source>Browse</source>
+        <comment>Browse files</comment>
+        <translation>Parcourir</translation>
+    </message>
+</context>
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="dialogabout.cpp" line="18"/>
+        <source>Visit website</source>
+        <translation>Visiter le site web</translation>
+    </message>
+    <message>
+        <location filename="dialogabout.cpp" line="22"/>
+        <source>About</source>
+        <translation>À Propos</translation>
+    </message>
+    <message>
+        <location filename="dialogabout.cpp" line="25"/>
+        <source>%1 is a light, flexible and cross platform quantity converter.&lt;br /&gt;Version %2.</source>
+        <translation>%1 est un convertisseur de quantités léger, fléxible et multiplate-forme.&lt;br /&gt;Version %2.</translation>
+    </message>
+</context>
+<context>
+    <name>DialogSettings</name>
+    <message>
+        <location filename="dialogsettings.cpp" line="56"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="59"/>
+        <source>Data</source>
+        <translation>Données</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="60"/>
+        <source>Window</source>
+        <translation>Fenêtre</translation>
+    </message>
+</context>
+<context>
+    <name>DialogTabData</name>
+    <message>
+        <location filename="dialogsettings.cpp" line="15"/>
+        <source>Data location</source>
+        <translation>Emplacement des données</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="16"/>
+        <source>Icons location</source>
+        <translation>Emplacement des icônes</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="17"/>
+        <source>Any change will take effect after restarting the application.</source>
+        <translation>Tout changement prendra effet après redémarrage du logiciel.</translation>
+    </message>
+</context>
+<context>
+    <name>DialogTabWindow</name>
+    <message>
+        <location filename="dialogsettings.cpp" line="33"/>
+        <source>Keep window state</source>
+        <translation>Conserver l&apos;état de la fenêtre</translation>
+    </message>
+    <message>
+        <location filename="dialogsettings.cpp" line="34"/>
+        <source>Reset window</source>
+        <translation>Réinitialiser la fenêtre</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="41"/>
+        <source>&amp;Application</source>
+        <translation>A&amp;pplication</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="44"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="42"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>About</source>
+        <translation>À Propos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>About Qt</source>
+        <translation>À Propos de Qt</translation>
+    </message>
+</context>
+<context>
+    <name>StackedWidget</name>
+    <message>
+        <location filename="stackedwidget.cpp" line="15"/>
+        <source>No quantities found.</source>
+        <translation>Aucune quantité trouvée.</translation>
+    </message>
+</context>
+</TS>
